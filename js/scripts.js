1ca6567d7259eeff64122a672300792e952b091e
marufacoder <marufaaktherjuthi@gmail.com>
2024-11-17 15:21:49 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// This function is to show hello world
+
+
+/*\
+ 1, dfdff
+ 3 dfd
+4. multiline comments
+
+*/
+//dfdfd
+
+// var firstName = "marufa"; 
+// var lastName = "juthi"; 
+// var fullName= firstName + " " + lastName;
+// console.log(fullName); 
+
+// var num1= 120;
+// var num2= 260;
+// var sum = num1+num2;
+// console.log (sum); 
+
+// var num1=2;
+// var num2=3;
+// var multiplication=num1*num2;
+// console.log(multiplication)
+
+// var num3=10;
+// var num4=20;
+// var num5=2;
+// var multiplication=(num3*num4)/num5;
+
+function sum_numbers() {
+    var num1 = 5;
+    var num2 = 2;
+    var sum  = num1+num2;
+    console.log(sum);  }
+    
+    sum_numbers(5,2);
+    
+    function average ( number1,number2){
+        var result  = number1-number2;
+    console.log(result);
+
+
+    }
+
+    
+    average (1024,128);
+
+
+
+    function divisionNumbers( number1,number2){
+
+        var result  = number1/number2;
+        return(result);
+        var divisionResult = divisionNumbers(1024,24);
+
+    
+
+    }
+    alart(divisionResult);
+
+
+
+
